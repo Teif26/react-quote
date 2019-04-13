9c60c4f0656e7e733ec953e436bf63b4629bf786
Andrew Dinsmore <teif26@outlook.com>
2019-04-13 10:05:51 -0400

    Updates

---
+++ precache-manifest.cfb09164ea9f9b14a8b6112699df6081.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "641ed187562d7696030b",
+    "url": "/react-quote/static/css/main.ec0769b5.chunk.css"
+  },
+  {
+    "revision": "641ed187562d7696030b",
+    "url": "/react-quote/static/js/main.430ff9b1.chunk.js"
+  },
+  {
+    "revision": "6a2bd7daf470b56590d9",
+    "url": "/react-quote/static/js/runtime~main.a27baf1e.js"
+  },
+  {
+    "revision": "679b2ab0bf5fd1698668",
+    "url": "/react-quote/static/css/2.ab821dec.chunk.css"
+  },
+  {
+    "revision": "679b2ab0bf5fd1698668",
+    "url": "/react-quote/static/js/2.3e818db0.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-quote/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-quote/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-quote/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-quote/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-quote/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b9c243178e5d3fe3b441c422f7254022",
+    "url": "/react-quote/index.html"
+  }
+];
